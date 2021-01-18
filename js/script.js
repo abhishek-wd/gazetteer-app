f195decb2eae08fb9770d5aea5e78258217b0dd1
abhishek-wd <abhishek.jha.np@gmail.com>
2021-01-18 18:44:28 +0000

    layout first iteration

--- js/script.js
+++ js/script.js
@@ -1,12 +1,56 @@
-// Normal Map
+/* Map Base Layers */
 
-const mapUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
-const mapattr = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
+// Open Street Map
+const mapOpen = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+const attrOpen = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
 
-const tempLocation = [27.72, 85.33];
+// Wiki Map
+const mapWiki = 'https://maps.wikimedia.org/osm-intl/{z}/{x}/{y}{r}.png';
+const attrWiki = '<a href="https://wikimediafoundation.org/wiki/Maps_Terms_of_Use">Wikimedia</a>';
 
-const map = L.map('map').setView(tempLocation, 10);
+// Satellite Map
+const mapSat = 'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}';
+const attrSat = 'Tiles &copy; Esri &mdash;';
 
-const firstLayer = L.tileLayer(mapUrl, { attribution: mapattr, maxZoom: 19});
+// Night Map
+const mapDark = 'https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png';
+const attrDark = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>';
 
-map.addLayer(firstLayer);
+// Toner - can only be added as layer to another map
+const tileToner = 'https://stamen-tiles-{s}.a.ssl.fastly.net/toner/{z}/{x}/{y}{r}.{ext}';
+const attrToner = 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
+
+// Creating Tile Layers for Individual Maps
+const mainMap = L.tileLayer(mapOpen, { attribution: attrOpen }), 
+        wikiMap = L.tileLayer(mapWiki, { attribution: attrWiki }),
+        satMap = L.tileLayer(mapSat, { attribution: attrSat }),
+        darkMap = L.tileLayer(mapDark, { attribution: attrDark });
+
+
+// Main Map 
+const map = L.map('mapId', {
+    center: [27.72, 85.33],
+    zoom: 15,
+   layers: [mainMap]
+});
+
+//const map = L.map('mapId').setView([27.72, 85.33], 15);
+// mainMap.addTo(map);
+
+var baseMaps = {
+    "Default": mainMap,
+    "Clear Mode": wikiMap,
+    "Night Mode": darkMap,
+    "Satellite": satMap
+}
+
+L.control.layers(baseMaps, null, {position: 'bottomleft'}).addTo(map);
+
+// Changing the default position of Zoom Control and Opacity
+map.zoomControl.setPosition('bottomright');
+L.DomUtil.setOpacity(map.zoomControl.getContainer(), 0.4);
+// L.DomUtil.addClass(map.zoomControl.getContainer(), 'zoom-control'); // Not Needed Anymore - Left for Reference
+
+// L.tileLayer(mapUrl, { attribution }).addTo(map);
+
+
